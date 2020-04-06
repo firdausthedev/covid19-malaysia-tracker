97ff92aa4fcc4b1ece8ce6b91da4712b2554c8f5
firdauscomel <bilbofirdaus@gmail.com>
2020-04-06 22:02:02 +0800

    fix bug

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Home />>
+          <Home />
         </div>
       </div>
     </Router>
