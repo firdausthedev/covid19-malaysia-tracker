09bd52d612afc57fd49411c3860a4518bee63cb1
firdauscomel <bilbofirdaus@gmail.com>
2020-04-06 21:54:14 +0800

    Updates

---
+++ precache-manifest.81ca877f8425c1debdf30da132f37871.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe609017189b042cabe77ebe8e7936b",
+    "url": "/covid19-malaysia-tracker/index.html"
+  },
+  {
+    "revision": "3aa526a1f901dbb925ac",
+    "url": "/covid19-malaysia-tracker/static/css/main.d2d188c0.chunk.css"
+  },
+  {
+    "revision": "1a97ab4e382d71eacb30",
+    "url": "/covid19-malaysia-tracker/static/js/2.ebfa885d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19-malaysia-tracker/static/js/2.ebfa885d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aa526a1f901dbb925ac",
+    "url": "/covid19-malaysia-tracker/static/js/main.8bc9cfdf.chunk.js"
+  },
+  {
+    "revision": "2cdfafec3b7136f0b2c3",
+    "url": "/covid19-malaysia-tracker/static/js/runtime-main.d93857a9.js"
+  }
+]);
