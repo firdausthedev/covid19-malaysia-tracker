0e9de68a5f1972399f3a0bca921a834d41f4a6ca
firdauscomel <bilbofirdaus@gmail.com>
2020-04-11 01:06:20 +0800

    added graphs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import './App.css';
 import Navbar from './components/layout/Nabvar';
@@ -11,8 +11,11 @@ function App() {
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/graph' component={Graph} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/graph' component={Graph} />
+            <Route component={Home} />
+          </Switch>
         </div>
       </div>
     </HashRouter>
