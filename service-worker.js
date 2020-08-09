54af8b02fae01516242e9cfdbbe59f488db667e9
norsevikingdev <norsevikingdev@gmail.com>
2020-08-09 15:10:06 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-malaysia-tracker/precache-manifest.2ba68980a70bedf2ef9b3d3b7e72ea8e.js"
+  "/covid19-malaysia-tracker/precache-manifest.4544076b3f49b78bf6e7392559f64691.js"
 );
 
 self.addEventListener('message', (event) => {
